b4890837bd400f894c8b0f2027ccf7d5455f19b0
Sonu Shivcharan <sonushivcharan@outlook.com>
2024-02-29 22:20:33 +0530

    update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function renderProdItems(products){
   const productsContainer = document.getElementById("products-container");
   products.map((item)=>{
     productsContainer.innerHTML+=`<div class="product">
-    <div class="prod-img"></div> 
+    <div class="prod-img" style="background-image:url('${item.img.url}')"></div> 
     <div class="details">
     <h4>${item.name}</h4>
     <p>&#8377;${item.price}</p>
