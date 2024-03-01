bb8bee2966e77c0815c462d5b8739945183da8c5
sonu-shivcharan <sonushivcharan@outlook.com>
2024-03-01 17:58:19 +0530

    added sections

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
+
 let products;
+let modal;
 (function(){
   fetch("./src/product.json")
 .then((resp)=>resp.json())
@@ -12,7 +14,7 @@ let products;
 function renderProdItems(products){
   const productsContainer = document.getElementById("products-container");
   products.map((item)=>{
-    productsContainer.innerHTML+=`<div class="product">
+    productsContainer.innerHTML+=`<div class="product" onclick='openProduct(${JSON.stringify(item)})'>
     <div class="prod-img" style="background-image:url('${item.img.url}')"></div> 
     <div class="details">
     <h4>${item.name}</h4>
@@ -21,4 +23,29 @@ function renderProdItems(products){
     </div>`
     console.log(item);
   });
+}
+function openProduct(item){
+  console.log(item);
+  const productPanel = document.createElement("div");
+  modal = document.createElement("div");
+  modal.setAttribute("id", "modal");
+  modal.addEventListener("click", closeModal);
+  productPanel.setAttribute("id", "product-panel");
+  productPanel.innerHTML=`<div>
+  <div class="img-container">
+    <img src=${item.img.url} alt=${item.name} />
+  </div>
+  <div class="details">
+  <h3>${item.name}</h3>
+  <p>Price : &#8377;${item.price}</p>
+  </div>
+  </div>`;
+  modal.appendChild(productPanel);
+  document.body.appendChild(modal);
+  console.log(modal);
+}
+function closeModal(e){
+  if(e.target.id=="modal"){
+    document.body.removeChild(modal);
+  }
 }
